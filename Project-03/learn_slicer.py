23b6d56dc2c072414d80665a7877f1ee2d723978
Shovon Das <Shovondas26@gmail.com>
2020-05-04 00:28:21 +0600

    slicer like js

---
+++ Project-03/learn_slicer.py
@@ -0,0 +1,23 @@
+# text
+x = "0123456789"
+
+# letter in position
+# x = x[1]
+# x = x[-1] #last letter
+
+# [start:end:step]
+# x = x[0:6]
+# x = x[0:6:2]
+# x = x[:5]
+# x = x[5:]
+
+# Reverse
+# x = x[::-1]
+
+
+# autometic slice
+x = x[x.index('2'):x.index("6")]
+
+
+# output
+print(x)
