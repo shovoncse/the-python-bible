5c296959947dbe3c6d79ba2afac9479376cf2828
Shovon Das <Shovondas26@gmail.com>
2020-05-03 23:11:35 +0600

    Projecta 2 on Strings & inputs

---
+++ project-02/Hello_You.py
@@ -0,0 +1,17 @@
+# Ask user for Name
+name = input("1. What is your name? \nAns:")
+
+# Ask User for Age
+age = input("1. What is your age? \nAns:")
+
+# Ask user for City
+city = input("1. What is your city? \nAns:")
+
+# Ask user what they enjoy
+enjoy = input("1. What do you enjoy? \nAns:")
+
+# Create output text
+output = f'Hi there, My name is {name}, i\'m {age} , i live in {city} and love to {enjoy}'
+
+# Print Output to the screen
+print(output)
