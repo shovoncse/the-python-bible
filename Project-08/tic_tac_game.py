b328d88e062918cc40be86575418d703639ce453
Shovon Das <Shovondas26@gmail.com>
2020-05-07 00:27:27 +0600

    comment added

--- Project-08/tic_tac_game.py
+++ Project-08/tic_tac_game.py
@@ -12,10 +12,13 @@ def game_status_disp():
     print(f'| {status[6]} | {status[7]} | {status[8]} |')
     print()
 
-# Get input
+# Get input method
 def get_input(c_player):
+
+    # user input
     user_input = input(f'{c_player}: Your turn, Enter Position (0-9): ').strip()
 
+    # Check for win result
     if user_input.isnumeric() and int(user_input) > 0 and int(user_input) < 10:
         if status[int(user_input)-1] != " ":
             print('Wrong!!! Input already used')
@@ -26,11 +29,13 @@ def get_input(c_player):
         print('Wrong input!!!, Please try again')
         return get_input(c_player)
 
-# Game Resul Check
+# Game Result Check
 def game_status(*match_board):
     
+    # initial result
     result = "no"
 
+    # Check for win result
     for item_set in match_board:
         if status[item_set[0]] == status[item_set[1]] == status[item_set[2]] != " ":
             if status[item_set[0]] == 'x':
@@ -39,23 +44,34 @@ def game_status(*match_board):
                 result = "P2(o)"
         else:
             continue
-
+    
+    # Match draw
     if result == "no" and " " not in status:
         print("It's a Draw")
         return 1
     
+    # Continue
     if result == "no":
         return 0
+    # Winner Found
     else:
         print(f'Congrats {result}, You won the game')
         return 1
 
 
+# Game Stars
 while True:
+
+    # initial display
     game_status_disp()
+
+    # Current Player
     current_player = 'P1(x)'
+
+    # 9 times lifecycle
     for i in range(0,9):
 
+        # get input
         user_input = get_input(current_player)
 
         if current_player == "P1(x)":
@@ -65,11 +81,15 @@ while True:
             status[user_input] = "o"
             current_player = "P1(x)"
 
+        # Current Status
         game_status_disp()
+
+        # Search the result
         if game_status(*match_board):
             # clear and new game
             status = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
             break
+        # if no result yet
         else:
             continue
         
