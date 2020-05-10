d503f66f9a356519342564d25b638999022cbe8a
Shovon Das <Shovondas26@gmail.com>
2020-05-10 19:51:34 +0600

    now the inheritence in p10

--- Project-09/Coins.py
+++ Project-09/Coins.py
@@ -1,9 +1,10 @@
 from random import choice
 # Abstruct Class (Template for all this type class)
 class Coin:
-    def __init__(self, rare= False, clean = True):
+    def __init__(self, rare = False, clean = True, heads = True):
         self.is_rare = rare
         self.is_clean = clean
+        self.heads = heads
     
         if self.is_rare:
             self.value = self.original_value * 1.25
@@ -14,10 +15,12 @@ class Coin:
             self.color = self.clean_color
         else:
             self.value = self.rusty_color
-        
+
+    def flip(self):
+        self.heads = choice([True, False])
+
     def __del__(self):
         print("Coin Spent!")
-
 
 
 
