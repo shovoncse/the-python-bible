c3123eeb19c5f7b225eafe9a49db624c3f90e8ac
Shovon Das <Shovondas26@gmail.com>
2020-05-10 19:07:13 +0600

    basic properties

---
+++ Project-09/Coins.py
@@ -0,0 +1,11 @@
+class Pound:
+    value = 1.00
+    color = "Golden"
+    num_edges = 1
+    diameter = 22.5 #mm
+    thickness = 3.15 #mm
+    heads = True
+
+# coin1 = Pound()
+# coin1.color
+# coin1.color = "Greenish"
