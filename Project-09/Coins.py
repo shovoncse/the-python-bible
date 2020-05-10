c4645938aad6adbda7822a41fc0eb7f3681b0cc5
Shovon Das <Shovondas26@gmail.com>
2020-05-10 19:13:27 +0600

    constructor added

--- Project-09/Coins.py
+++ Project-09/Coins.py
@@ -1,10 +1,19 @@
 class Pound:
-    value = 1.00
-    color = "Golden"
-    num_edges = 1
-    diameter = 22.5 #mm
-    thickness = 3.15 #mm
-    heads = True
+
+    def __init__(self,rare=False):
+        self.value = 1.00
+        self.color = "Golden"
+        self.num_edges = 1
+        self.diameter = 22.5 #mm
+        self.thickness = 3.15 #mm
+        self.heads = True
+        
+        self.rare = rare
+        if self.rare:
+            self.value = 1.25
+        else:
+            self.value = 1.00
+    
 
 # coin1 = Pound()
 # coin1.color
